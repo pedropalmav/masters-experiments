141c68c774494d2726bad89f23281e18457699fa
pedropalmav <pedro.palma@uc.cl>
2025-10-14 19:14:14 -0300

    feat: Automate success rate

--- vit_bc/test_vit.py
+++ vit_bc/test_vit.py
@@ -1,4 +1,6 @@
+import argparse
 import gym
+import numpy as np
 import gym_sokoban
 import torch
 import os
@@ -6,36 +8,63 @@ import utils.observation_parser as observation_parser
 from utils.sokoban_visualizer import SokobanVisualizer
 from vit import ViTBC
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-vit = ViTBC(
-        image_size=8,
-        patch_size=1,
-        num_layers=4,
-        num_heads=4,
-        hidden_dim=128,
-        mlp_dim=256,
-        image_channels=7,
-        num_classes=5
-    )
-vit.load_state_dict(
-    torch.load(os.path.join(os.path.dirname(__file__), "models", "vit_one_trajectory.pth"), map_location=device)
-)
-vit.to(device)
-vit.eval()
+def test_level(env: gym.Env, room_id: int, render: bool = True):
+    obs = env.reset(room_id=room_id)
 
-env = gym.make("Sokoban-v0")
-obs = env.reset(room_id=0)
-
-visualizer = SokobanVisualizer()
-visualizer.render(obs)
-
-with torch.no_grad():
-    while True:
-        obs_tensor = torch.tensor(obs, dtype=torch.float32).unsqueeze(0).permute(0, 3, 1, 2).to(device)
-        action = vit(obs_tensor).argmax(dim=1).item()
-        obs, reward, done, _ = env.step(action)
-
+    if render:
+        visualizer = SokobanVisualizer()
         visualizer.render(obs)
 
-        if done:
-            break
+    with torch.no_grad():
+        while True:
+            obs_tensor = torch.tensor(obs, dtype=torch.float32).unsqueeze(0).permute(0, 3, 1, 2).to(device)
+            action = vit(obs_tensor).argmax(dim=1).item()
+            obs, reward, done, _ = env.step(action)
+
+            if render:
+                visualizer.render(obs)
+
+            if done:
+                break
+        
+    return success(obs)
+
+def success(obs: np.ndarray) -> int:
+    return int(not np.any(obs[:, :, 2]))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_levels", type=int, help="Number of levels to test")
+    parser.add_argument("--render", default=False, help="Render the environment")
+    parser.add_argument("--env_split", type=str, default="", help="Environment split")
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    vit = ViTBC(
+            image_size=8,
+            patch_size=1,
+            num_layers=2,
+            num_heads=2,
+            hidden_dim=64,
+            mlp_dim=128,
+            image_channels=7,
+            num_classes=5
+        )
+    vit.load_state_dict(
+        torch.load(os.path.join(os.path.dirname(__file__), "models", "vit_bc.pth"), map_location=device)
+    )
+    vit.to(device)
+    vit.eval()
+
+    middle = {
+        '': '-',
+        'valid': '-valid-',
+        'test': '-test-'
+    }
+    env = gym.make(f"Sokoban{middle[args.env_split]}v0")
+
+    total_success = 0
+    for level in range(args.num_levels):
+        total_success += test_level(env, level, args.render)
+    
+    print(f"Success rate: {total_success / args.num_levels:.2%}")
