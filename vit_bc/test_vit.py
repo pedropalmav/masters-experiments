923e76cd4d1ca6e9c83fbe877fec7ec46dd903d9
pedropalmav <pedro.palma@uc.cl>
2025-10-14 17:45:08 -0300

    feat: Add early stopping

--- vit_bc/test_vit.py
+++ vit_bc/test_vit.py
@@ -3,6 +3,7 @@ import gym_sokoban
 import torch
 import os
 import utils.observation_parser as observation_parser
+from utils.sokoban_visualizer import SokobanVisualizer
 from vit import ViTBC
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -16,21 +17,25 @@ vit = ViTBC(
         image_channels=7,
         num_classes=5
     )
-vit.load_state_dict(torch.load("vit_sokoban.pth", map_location=device))
+vit.load_state_dict(
+    torch.load(os.path.join(os.path.dirname(__file__), "models", "vit_one_trajectory.pth"), map_location=device)
+)
 vit.to(device)
 vit.eval()
 
 env = gym.make("Sokoban-v0")
 obs = env.reset(room_id=0)
-observation_parser.format_obs(obs)
-print()
+
+visualizer = SokobanVisualizer()
+visualizer.render(obs)
 
 with torch.no_grad():
     while True:
         obs_tensor = torch.tensor(obs, dtype=torch.float32).unsqueeze(0).permute(0, 3, 1, 2).to(device)
         action = vit(obs_tensor).argmax(dim=1).item()
         obs, reward, done, _ = env.step(action)
-        observation_parser.format_obs(obs)
-        print()
+
+        visualizer.render(obs)
+
         if done:
             break
--- vit_bc/train_vit.py
+++ vit_bc/train_vit.py
@@ -1,13 +1,33 @@
+import os
+from tqdm import tqdm
 import torch
-import torch.utils.data as data
+from torch.utils.data import DataLoader, Subset
 import numpy as np
 from vit import ViTBC
 from BCDataset import BCDataset
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-dataset = torch.load("data/sokoban_manual_data.pt", weights_only=False)
+dataset_path = os.path.join(os.path.dirname(__file__), "data", "train_trajectories_250m.pt")
+dataset = torch.load(dataset_path, weights_only=False)
 print(f"Dataset size: {len(dataset)}")
-dataloader = data.DataLoader(dataset, batch_size=6, shuffle=True, num_workers=1)
+dataloader = DataLoader(dataset, batch_size=10, num_workers=1)
+
+dataset_size = len(dataset)
+train_size = int(0.7 * dataset_size)
+validation_size = int(0.15 * dataset_size)
+test_size = dataset_size - train_size - validation_size
+
+train_indices = list(range(0, train_size))
+validation_indices = list(range(train_size, train_size + validation_size))
+test_indices = list(range(train_size + validation_size, dataset_size))
+
+train_dataset = Subset(dataset, train_indices)
+validation_dataset = Subset(dataset, validation_indices)
+test_dataset = Subset(dataset, test_indices)
+
+train_dataloader = DataLoader(train_dataset, batch_size=10, shuffle=True, num_workers=1)
+validation_dataloader = DataLoader(validation_dataset, batch_size=10, shuffle=False, num_workers=1)
+test_dataloader = DataLoader(test_dataset, batch_size=10, shuffle=False, num_workers=1)
 
 vit = ViTBC(
         image_size=8,
@@ -21,23 +41,52 @@ vit = ViTBC(
     )
 
 vit.to(device)
+
 optimizer = torch.optim.Adam(vit.parameters(), lr=1e-4)
 criterion = torch.nn.CrossEntropyLoss()
-num_epochs = 200
-vit.train()
+num_epochs = 20
+best_val_loss = float("inf")
+patience = 5
+patience_counter = 0
+
+
 for epoch in range(num_epochs):
-    total_loss = 0.0
-    for batch_idx, (inputs, targets) in enumerate(dataloader):
+    vit.train()
+    total_train_loss = 0.0
+    for batch_idx, (inputs, targets) in tqdm(enumerate(train_dataloader), total=len(train_dataloader)):
         inputs, targets = inputs.to(device), targets.to(device)
-        inputs = inputs.permute(0, 3, 1, 2)  # Change shape from (B, H, W, C) to (B, C, H, W)
         inputs = inputs.float()
         optimizer.zero_grad()
         outputs = vit(inputs)
         loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
-        total_loss += loss.item()
-    avg_loss = total_loss / len(dataloader)
-    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}")
+        total_train_loss += loss.item()
 
-torch.save(vit.state_dict(), "vit_sokoban.pth")
+    vit.eval()
+    total_val_loss = 0.0
+    with torch.no_grad():
+        for inputs, targets in validation_dataloader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            inputs = inputs.float()
+            outputs = vit(inputs)
+            loss = criterion(outputs, targets)
+            total_val_loss += loss.item()
+
+    avg_train_loss = total_train_loss / len(train_dataloader)
+    avg_val_loss = total_val_loss / len(validation_dataloader)
+    print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}")
+
+    if avg_val_loss < best_val_loss:
+        best_val_loss = avg_val_loss
+        patience_counter = 0
+        torch.save(vit.state_dict(), os.path.join(os.path.dirname(__file__), "models", "vit_bc.pth"))
+    else:
+        patience_counter += 1
+    
+    if patience_counter >= patience:
+        print("Early stopping triggered")
+        break
+
+
+torch.save(vit.state_dict(), os.path.join(os.path.dirname(__file__), "models", "vit_bc.pth"))
